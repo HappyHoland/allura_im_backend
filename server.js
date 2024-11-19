6ae9b2c5d0e13096589121b06e066457d99895ba
José Felipe Holanda <josefelipehol@gmail.com>
2024-11-19 20:02:03 -0300

    "feat: adicionadas mudanças da aula 2"

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 import express from "express";
 
+const posts = [
+    {id: 1, desc: "descrição A - AAA", img: "sample1.png"},
+    {id: 2, desc: "descrição B - BBB", img: "sample2.png"},
+    {id: 3, desc: "descrição C - CCC", img: "sample3.png"},
+];
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req,res) => {
-    res.status(200).send("Hello World!");
+function getPostById(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts", (req,res) => {
+    res.status(200).json(posts);
 });
+
+app.get("/posts/:id", (req, res) => {
+    const index = getPostById(req.params.id);
+
+    res.status(200).json(posts[index]);
+})
