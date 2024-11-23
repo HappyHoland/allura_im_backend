6c87976207e90456e01e8fe4e462960a4a11ac04
José Felipe Holanda <josefelipehol@gmail.com>
2024-11-22 23:05:40 -0300

    feat: granted public access to local folder 'uploads'

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import routes from "./src/routes/postsRoutes.js"
 
 const app = express();
 
+app.use(express.static("uploads"));
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
